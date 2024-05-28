11958267a9b1ab8b770f161cb26933e22ec8b002
BlueNakoTech <118507068+BlueNakoTech@users.noreply.github.com>
2024-05-28 15:50:18 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,16 +4,17 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"android-chrome-192x192.png": "1ce5d96ee26b2fbb5a116c7dd2204a77",
-"assets/AssetManifest.bin": "262afa84277d8cd47d64a47131cd813b",
-"assets/AssetManifest.json": "d6a5aa29567fe9db897013f5fa3d3a1f",
+"assets/AssetManifest.bin": "91db1a0899a6abd77df9ae19676fce3f",
+"assets/AssetManifest.json": "5683b4ee4c698c6feaf86225ddaf3615",
 "assets/assets/data/data.json": "40c59fb6a2c6cc85537511087d0751b0",
+"assets/assets/images/background.webp": "0aee43ef4d77076f9c9245b61ac8699e",
 "assets/assets/images/LOGO_ODIN_PNG.png": "eb378ccfe6feb3d611d9da3748ddc924",
 "assets/assets/images/Logo_QED.png": "bd235b05eab167ace25a9edeb54a90ab",
 "assets/assets/images/Logo_tKRI.png": "52ac39c8a89f4b5dc8ebc1bf7e73e763",
 "assets/assets/images/ShirokoLogo.png": "b1bcb500781c8fc60b2a2c6d3c589d10",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "32fce58e2acb9c420eab0fe7b828b761",
-"assets/NOTICES": "1657df5f1228cdef16856daa1a851fcf",
+"assets/NOTICES": "864f53846b403c6235c3b4e5c116249b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
 "canvaskit/canvaskit.js": "bbf39143dfd758d8d847453b120c8ebb",
@@ -32,9 +33,9 @@ const RESOURCES = {"android-chrome-192x192.png": "1ce5d96ee26b2fbb5a116c7dd2204a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "34686c5d0611b235da064225c5f5b909",
-"/": "34686c5d0611b235da064225c5f5b909",
-"main.dart.js": "6fecfae2789865d47cede4d853b66621",
+"index.html": "5d9c43f38fb7372d318db79dd521edad",
+"/": "5d9c43f38fb7372d318db79dd521edad",
+"main.dart.js": "2a46678efa2e8ae11e5956f808818172",
 "manifest.json": "051e8ab39ee842bf0e9d890ee2add9f4",
 "version.json": "863294f3282a0e4c0b2a07cdf53658d8"};
 // The application shell files that are downloaded before a service worker can
